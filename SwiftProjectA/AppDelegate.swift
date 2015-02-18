fba88c99e4b4310771f06ec6fb8a42669c80e2d8
David Raftis <dpr11@mac.com>
2015-02-18 18:07:54 +0000

    Initial Commit

---
+++ SwiftProjectA/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftProjectA
+//
+//  Created by David Raftis on 18/02/2015.
+//  Copyright (c) 2015 Spelga Software Limited. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
